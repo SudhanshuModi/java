b6561902d785b823be02b225bd3f96a33582ee7b
Sudhanshu <sudhanshu565868@gmail.com>
2025-01-29 15:26:25 +0000

    while and do-while loop

--- Basics/Loops.java
+++ Basics/Loops.java
@@ -2,8 +2,30 @@ package Basics;
 
 public class Loops {
     public static void main(String args[]) {
+        // forLoop();
+        // whileLoop();
+        doWhile();
+    }
+
+    public static void forLoop() {
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
         }
+    }
+
+    public static void whileLoop() {
+        int i = 1;
+        while (i <= 6) {
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void doWhile() {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i == 7);
     }
 }
