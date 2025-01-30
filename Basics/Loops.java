c477be90f766e4578f3b7fc7035c4e3ef3b57c83
Sudhanshu <sudhanshu565868@gmail.com>
2025-01-30 10:39:05 +0000

    menuDrivenProgram

--- Basics/Loops.java
+++ Basics/Loops.java
@@ -7,11 +7,14 @@ public class Loops {
         // forLoop();
         // whileLoop();
         // doWhileLoop();
-        
+
         Scanner sc = new Scanner(System.in);
         // SumOfNaturalNo(sc);
         // tableOfNumber(sc);
-        evenNumberTillN(sc);
+
+        // Home Work Section
+        // evenNumberTillN(sc);
+        menuDrivenProgram(sc);
     }
 
     public static void forLoop() {
@@ -50,23 +53,55 @@ public class Loops {
     }
 
     // Print the table of a number input by the user.
-    public static void tableOfNumber(Scanner sc){
+    public static void tableOfNumber(Scanner sc) {
         int n = sc.nextInt();
 
-        for(int i = 1; i<=10; i++){
-            System.out.println(n *i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n * i);
         }
     }
 
+    // Home Work Section
     // Print all even numbers till n.
-    public static void evenNumberTillN(Scanner sc){
+    public static void evenNumberTillN(Scanner sc) {
         int n = sc.nextInt();
 
-        for(int i = 2; i<=n; i++){
-            if(i%2 == 0){
+        for (int i = 2; i <= n; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
     }
 
+    /*
+     * Make a menu driven program. The user can enter 2 numbers, either 1 or 0.
+     * If the user enters 1 then keep taking input from the user for a student’s
+     * marks(out of 100).
+     * If they enter 0 then stop.
+     * If he/ she scores :
+     * Marks >=90 -> print “This is Good”
+     * 89 >= Marks >= 60 -> print “This is also Good”
+     * 59 >= Marks >= 0 -> print “This is Good as well”
+     * Because marks don’t matter but our effort does.
+     * (Hint : use do-while loop but think & understand why)
+     */
+    public static void menuDrivenProgram(Scanner sc) {
+        System.out.println("Enter 0 or 1");
+        int input = sc.nextInt();
+        do {
+            System.out.println("Enter marks");
+            int marks = sc.nextInt();
+
+            if (marks >= 90) {
+                System.out.println("This is Good");
+            } else if (marks <= 89 && marks >= 60) {
+                System.out.println("This is also Good");
+            } else if (marks <= 59 && marks >= 0) {
+                System.out.println("This is Good as well");
+            }
+
+            System.out.println("Enter 0 or 1");
+            input = sc.nextInt();
+        } while (input == 1);
+    }
 }
