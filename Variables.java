c1d79f64cf263ff266c744b3be654c18ebe4e3b7
Sudhanshu <sudhanshu565868@gmail.com>
2025-01-30 11:08:46 +0000

    changes

--- Variables.java
+++ Variables.java
@@ -1,21 +1,20 @@
-package Basics;
 
 import java.util.Scanner;
 
 public class Variables {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("Hello World with java");
 
         String name = "sudhanshu modi";
         Integer age = 24;
         Double price = 50.5;
-        
+
         price = 50000.5;
 
         System.out.println(name);
         System.out.println(age);
         System.out.println(price);
-        
+
         Scanner sc = new Scanner(System.in);
         String newName = sc.nextLine();
         System.out.println(newName);
