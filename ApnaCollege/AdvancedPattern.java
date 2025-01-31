4b3918a1f66a53f9ee5832b73db9a409efe1768b
Sudhanshu <sudhanshu565868@gmail.com>
2025-01-31 08:49:27 +0000

    home work for advanced pattern

--- ApnaCollege/AdvancedPattern.java
+++ ApnaCollege/AdvancedPattern.java
@@ -6,7 +6,14 @@ public class AdvancedPattern {
         // solidRhombus();
         // numberPyramid();
         // palindromicPattern();
-        diamondPattern();
+        // diamondPattern();
+
+        // Home Work
+        // hollowButterfly();
+        // hollowRhombus();
+        // pascalsTriangle();
+        // halfPyramid();
+        invertedHalfPyramid();
     }
 
     public static void butterflyPattern() {
@@ -101,6 +108,111 @@ public class AdvancedPattern {
             }
             for (int j = 1; j <= i * 2 - 1; j++) {
                 System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // Home Work
+    public static void hollowButterfly() {
+        int num = 4;
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for (int j = 1; j <= 2 * (num - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = num; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for (int j = 1; j <= 2 * (num - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void hollowRhombus() {
+        int num = 5;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= num; j++) {
+                if (i == 1 || i == num || j == 1 || j == num) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pascalsTriangle() {
+        int num = 5;
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print(" ");
+            }
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void halfPyramid() {
+        int num = 5;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void invertedHalfPyramid() {
+        int num = 5;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num - i; j++) {
+                System.out.print(i);
             }
             System.out.println();
         }
